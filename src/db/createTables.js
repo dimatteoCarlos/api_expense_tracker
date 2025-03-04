b423490c9e180d44ce5437feaa70380154f1f3b5
CarlosDR <dimatteoca@gmail.com>
2025-03-04 19:38:31 -0400

    recordTransaction initiated

--- src/db/createTables.js
+++ src/db/createTables.js
@@ -2,7 +2,7 @@ export const createTables = `
 CREATE TABLE users(
 user_id UUID PRIMARY KEY NOT NULL, username VARCHAR(50) UNIQUE NOT NULL, email VARCHAR(50) UNIQUE NOT NULL, user_firstname VARCHAR(25) NOT NULL,  user_lastname VARCHAR(25)  NOT NULL, user_contact VARCHAR(25), password_hashed VARCHAR(255) NOT NULL, currency_id INT DEFAULT 1 REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-user_role_id INT DEFAULT REFERENCES user_roles(user_role_id) ON DELETE SET NULL ON UPDATE CASCADE,
+user_role_id INT DEFAULT REFERENCES user_roles(user_ IF NOT EXISTSrole_id) ON DELETE SET NULL ON UPDATE CASCADE,
 ) 
 
 ALTER TABLE users ADD COLUMN user_role_id INT REFERENCES user_roles(user_role_id) ON DELETE SET NULL ON UPDATE CASCADE
@@ -19,7 +19,7 @@ CREATE TABLE account_types (
     account_type_name VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE user_accounts (
+CREATE TABLE IF NOT EXISTS user_accounts (
 account_id SERIAL PRIMARY KEY NOT NULL, 
 user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
 account_name VARCHAR(50) NOT NULL,
@@ -28,17 +28,17 @@ currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT O
  account_starting_amount DECIMAL(15,2) NOT NULL,
  account_balance DECIMAL(15,2) NOT NULL DEFAULT 0.00,
     account_start_date TIMESTAMPTZ NOT NULL CHECK (account_start_date <= NOW()) , 
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMPT,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMPT
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 
 CREATE TABLE movement_types (
-movement_type_id SERIAL PRIMARY KEY NOT NULL, movement_type_name VARCHAR(50) NOT NULL CHECK (movement_type_name IN ('expense', 'income', 'investment', 'debt', 'pocket')
+movement_type_id SERIAL PRIMARY KEY NOT NULL, movement_type_name VARCHAR(15) NOT NULL CHECK (movement_type_name IN ('expense', 'income', 'investment', 'debt', 'pocket', 'transfer', 'receive')
 ))
 
-CREATE TABLE movements (
+CREATE TABLE IF NOT EXISTS movements (
     movement_id SERIAL PRIMARY KEY NOT NULL,
-    movement_type_id INT NOT NULL REFERENCES movement_types(movement_type_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    movement_type_id INT NOT NULL REFERENCES movement_types(movement_type_id) ON DELETE SET NULL ON UPDATE CASCADE,
     user_id UUID NOT NULL REFERENCES "users"(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, 
     amount DECIMAL(15,2) NOT NULL,
@@ -47,51 +47,50 @@ CREATE TABLE movements (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 
-CREATE TABLE expense_movements (
+CREATE TABLE IF NOT EXISTS expense_movements (
     movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    category_id INT NOT NULL REFERENCES expense_category(category_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    category_id INT NOT NULL REFERENCES expense_categories(category_id) ON DELETE CASCADE ON UPDATE CASCADE,
     account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
-CREATE TABLE expense_categories ( 
+CREATE TABLE IF NOT EXISTS expense_categories ( 
     category_id SERIAL PRIMARY KEY NOT NULL,
     category_name VARCHAR(50) NOT NULL,
     nature_name VARCHAR(8) NOT NULL CHECK (nature_name IN ('must', 'need', 'other', 'want')),
     budget DECIMAL(15,2) NOT NULL
 )
 
-CREATE TABLE income_movements (
+CREATE TABLE IF NOT EXISTS income_movements (
     movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
     source_id INT NOT NULL REFERENCES expense_categories(category_id) ,
     account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
-CREATE TABLE income_sources (
+CREATE TABLE IF NOT EXISTS income_sources (
     source_id SERIAL PRIMARY KEY NOT NULL,
     source_name VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE investment_movements (
+CREATE TABLE IF NOT EXISTS investment_movements (
     movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
     transaction_type VARCHAR(10) NOT NULL CHECK (transaction_type IN ('deposit', 'withdraw')),
     transaction_investment_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
-CREATE TABLE debt_movements (
+CREATE TABLE IF NOT EXISTS debt_movements (
     movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    debtor_id INT NOT NULL REFERENCES debt_debtor(debtor_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    debtor_id INT NOT NULL REFERENCES debt_debtors(debtor_id) ON DELETE CASCADE ON UPDATE CASCADE,
     debt_transaction_type VARCHAR(8) NOT NULL CHECK (debt_transaction_type IN ('lend', 'borrow')),
-    transaction_debt_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+    transaction_debt_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP)
 
-CREATE TABLE debt_debtors (
+CREATE TABLE IF NOT EXISTS debt_debtors (
     debtor_id SERIAL PRIMARY KEY NOT NULL,
     debtor_name VARCHAR(25) NOT NULL,
     debtor_lastname VARCHAR(25) NOT NULL
 )
 
-CREATE TABLE pocket_movements (
+CREATE TABLE IF NOT EXISTS pocket_movements (
     pocket_id SERIAL PRIMARY KEY NOT NULL,
     movement_id INT NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
     pocket_name VARCHAR(50) NOT NULL,
@@ -106,12 +105,139 @@ user_id UUID NOT NULL,
 description TEXT,
 amount DECIMAL(15,2) NOT NULL, 
 movement_type_id INTEGER NOT NULL,
-transaction_type_id INTEGER NOT NULL
+transaction_type_id INTEGER NOT NULL,
 currency_id INTEGER NOT NULL, 
-source_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE, 
-destination_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE, 
+ source_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
+destination_account_id INT  , 
 status VARCHAR(50) NOT NULL, 
 transaction_actual_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 )
+
+CREATE TABLE IF NOT EXISTS transaction_types(
+transaction_type_id SERIAL PRIMARY KEY NOT NULL,
+transation_type_name VARCHAR(50) NOT NULL CHECK(transation_type_name IN ('withdraw', 'deposit', 'lend', 'borrow'))
+)
 `;
+
+export const createMainTables = [
+  {
+    tblName: 'UserAccounts',
+    table: `
+CREATE TABLE IF NOT EXISTS user_accounts (
+account_id SERIAL PRIMARY KEY NOT NULL, 
+user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+account_name VARCHAR(50) NOT NULL,
+account_type_id INT  REFERENCES account_types(account_type_id) ON DELETE SET NULL ON UPDATE CASCADE, 
+currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT ON UPDATE CASCADE, 
+ account_starting_amount DECIMAL(15,2) NOT NULL,
+ account_balance DECIMAL(15,2) NOT NULL DEFAULT 0.00,
+    account_start_date TIMESTAMPTZ NOT NULL CHECK (account_start_date <= NOW()) , 
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+    `,
+  },
+  {
+    tblName: 'Movements',
+    table: `CREATE TABLE IF NOT EXISTS movements (
+    movement_id SERIAL PRIMARY KEY NOT NULL,
+    movement_type_id INT NOT NULL REFERENCES movement_types(movement_type_id) ON DELETE SET NULL ON UPDATE CASCADE,
+    user_id UUID NOT NULL REFERENCES "users"(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, 
+    amount DECIMAL(15,2) NOT NULL,
+    description VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+)`,
+  },
+  {
+    tblName: 'ExpenseMovements',
+    table: `CREATE TABLE IF NOT EXISTS expense_movements (
+    movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    category_id INT NOT NULL REFERENCES expense_categories(category_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE
+)`,
+  },
+
+  {
+    tblName: 'ExpenseCategories',
+    table: `CREATE TABLE IF NOT EXISTS expense_categories( 
+    category_id SERIAL PRIMARY KEY NOT NULL,
+    category_name VARCHAR(50) NOT NULL,
+    nature_name VARCHAR(8) NOT NULL CHECK (nature_name IN ('must', 'need', 'other', 'want')),
+    budget DECIMAL(15,2) NOT NULL
+)`,
+  },
+
+  {
+    tblName: 'IncomeMovements',
+    table: `CREATE TABLE IF NOT EXISTS income_movements (
+    movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    source_id INT NOT NULL REFERENCES expense_categories(category_id),
+    account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE
+)`,
+  },
+  {
+    tblName: 'IncomeSources',
+    table: `CREATE TABLE IF NOT EXISTS income_sources (
+    source_id SERIAL PRIMARY KEY NOT NULL,
+    source_name VARCHAR(50) NOT NULL)`,
+  },
+  {
+    tblName: 'Investment_movements',
+    table: `CREATE TABLE IF NOT EXISTS investment_movements (
+    movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    transaction_type VARCHAR(10) NOT NULL CHECK (transaction_type IN ('deposit', 'withdraw')),
+    transaction_investment_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE
+)`,
+  },
+  {
+    tblName: 'Debt_movements',
+    table: `CREATE TABLE IF NOT EXISTS debt_movements (
+    movement_id INT PRIMARY KEY NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    debtor_id INT NOT NULL REFERENCES debt_debtors(debtor_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    debt_transaction_type VARCHAR(8) NOT NULL CHECK (debt_transaction_type IN ('lend', 'borrow')),
+    transaction_debt_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP)`,
+  },
+  {
+    tblName: 'Debt_debtors',
+    table: `CREATE TABLE IF NOT EXISTS debt_debtors (
+    debtor_id SERIAL PRIMARY KEY NOT NULL,
+    debtor_name VARCHAR(25) NOT NULL,
+    debtor_lastname VARCHAR(25) NOT NULL
+    )`,
+  },
+  {
+    tblName: 'Pocket_movements',
+    table: `CREATE TABLE IF NOT EXISTS pocket_movements (
+    pocket_id SERIAL PRIMARY KEY NOT NULL,
+    movement_id INT NOT NULL REFERENCES movements(movement_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    pocket_name VARCHAR(50) NOT NULL,
+    target_amount DECIMAL(15,2),
+    pocket_note VARCHAR(50),
+    desired_date TIMESTAMP,
+)`,
+  },
+  {
+    tblName: 'Transactions',
+    table: `CREATE TABLE IF NOT EXISTS transactions(
+transaction_id SERIAL PRIMARY KEY,
+user_id UUID NOT NULL,
+description TEXT,
+amount DECIMAL(15,2) NOT NULL, 
+movement_type_id INTEGER NOT NULL,
+transaction_type_id INTEGER NOT NULL,
+currency_id INTEGER NOT NULL, 
+ source_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
+destination_account_id INT  , 
+status VARCHAR(50) NOT NULL, 
+transaction_actual_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+)`,
+  },
+];
+
+// source_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
+// destination_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
---
+++ utils/recordTransaction.js
@@ -0,0 +1,58 @@
+//recordTransaction.js
+
+import pc from 'picocolors';
+import { pool } from '../src/db/configDB.js';
+import { handlePostgresError } from './errorHandling.js';
+
+export async function recordTransaction(options) {
+  // const client = await pool.connect();
+  try {
+    const {
+      userId,
+      description,
+      movement_type_id,
+      status,
+      amount,
+      currency_id,
+      source_account_id,
+      transaction_type_id,
+      destination_account_id,
+      transaction_actual_date,
+    } = options;
+    console.log("🚀 ~ recordTransaction ~ options:", options)
+
+    //start the transaction
+    // await client.pool.query('BEGIN');
+
+    const transactionResult = await pool.query({
+      text: `INSERT INTO transactions(user_id, description, movement_type_id, status, amount,currency_id, source_account_id,transaction_type_id,destination_account_id, transaction_actual_date) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *`,
+      values: [
+        userId,
+        description,
+        movement_type_id,
+        status,
+        amount,
+        currency_id,
+        source_account_id,
+        transaction_type_id,
+        destination_account_id,
+        transaction_actual_date,
+      ],
+    });
+
+    // console.log(transactionResult.rows[0]);
+
+    // await client.query('COMMIT');
+
+    const message = 'Transaction completed successfully.';
+    console.log(pc.yellowBright(message));
+    // res.status(200).json({ status: 200, message });
+
+    return transactionResult.rows[0];
+  } catch (error) {
+    // await client.query('ROLLBACK');
+    const message = error.message || `Error when recording transaction.`;
+    console.error(pc.redBright(message), 'from record transaction');
+    throw handlePostgresError(error);
+  } 
+}
